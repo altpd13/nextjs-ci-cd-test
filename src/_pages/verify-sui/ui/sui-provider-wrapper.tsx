7dbd30a9eb030be11688db28f1c8c830ebce5e72
altpd13 <altpd13@gmail.com>
2024-10-31 18:08:57 +0900

    add: add sui provider wrapper

---
+++ src/_pages/verify-sui/ui/sui-provider-wrapper.tsx
@@ -0,0 +1,27 @@
+import {
+  SuiClientProvider,
+  WalletProvider,
+  createNetworkConfig,
+} from "@mysten/dapp-kit";
+import { getFullnodeUrl } from "@mysten/sui/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const { networkConfig } = createNetworkConfig({
+  devnet: { url: getFullnodeUrl("devnet") },
+  testnet: { url: getFullnodeUrl("testnet") },
+  mainnet: { url: getFullnodeUrl("mainnet") },
+});
+
+const SuiProviderWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+        <WalletProvider>{children}</WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
+  );
+};
+
+export default SuiProviderWrapper;
